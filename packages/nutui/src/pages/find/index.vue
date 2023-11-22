0b124a3f19e08fa8abc4268a3340dcf384cd8e35
hy <iqszlong@qq.com>
2023-11-22 12:47:53 +0800

    调整首页内容，测试内容转到发现

--- packages/nutui/src/pages/find/index.vue
+++ packages/nutui/src/pages/find/index.vue
@@ -1,42 +1,149 @@
 <template>
   <view class="find">
     <TopBar class="top-bar-bg" :title="title" :setPlace="true"></TopBar>
+    <NutCellGroup :title="`版本号${global.version}`">
+      <NutCell title="404页面" is-link @click="goPage('/pages/notfind/index')"></NutCell>
+      <NutCell title="登录页" is-link @click="goPage('/pages/login/index')"></NutCell>
+    </NutCellGroup>
 
 
 
 
-    发现页面
-   
+    <NutCellGroup title='微信原生功能测试'>
+      <NutCell>
+        <NutSpace wrap>
+          <nut-button open-type="share">分享</nut-button>
+        </NutSpace>
+      </NutCell>
+    </NutCellGroup>
+    <NutCellGroup title='按钮效果'>
+      <NutCell>
+        <NutSpace wrap>
+          <nut-button type="primary" @click="handleClick('text', msg2, true)">信息提示toast</nut-button>
+          <nut-button @click="setRequest">发起测试请求</nut-button>
+        </NutSpace>
+      </NutCell>
+    </NutCellGroup>
+
+    <NutCellGroup title="第三方图标" desc="使用请打开app.js中remixicon.css注释。下方是图标效果。注意：使用后打包文件会增加100k">
+      <NutCell>
+        <NutSpace wrap>
+          <IconFont font-class-name="remixicon" class-prefix="ri" name="home-7-line"></IconFont>
+          <nut-button>
+            <template #icon>
+              <IconFont font-class-name="remixicon" class-prefix="ri" name="delete-bin-5-line"></IconFont>
+            </template>
+          </nut-button>
+        </NutSpace>
+      </NutCell>
+
+    </NutCellGroup>
+
+    <NutCellGroup title="原始图标">
+      <NutCell>
+        <NutSpace wrap>
+          <IconFont name="JD"></IconFont>
+        </NutSpace>
+      </NutCell>
+    </NutCellGroup>
+
 
     <nut-toast :msg="msg" v-model:visible="show" :type="type" :cover="cover" />
+
+
+
+    
   </view>
 </template>
 
 <script>
-import { reactive, toRefs } from 'vue';
+import { useShareAppMessage } from "@tarojs/taro";
+import { reactive, toRefs, onMounted, computed } from 'vue';
+import utils from "@/utils/index";
+import api from "@/api/";
+import { useSystemStore } from "@/store/index";
+import { IconFont } from '@nutui/icons-vue-taro';
+
 export default {
+  name: 'Index',
   components: {
+    IconFont
   },
   setup() {
+
+    const store = useSystemStore();
+
     const state = reactive({
-      title: '发现',
+      title: '发现',	
+      msg: '你成功了～',
+      type: 'text',
+      show: false,
+      cover: false
     });
 
-    const tips = reactive({
-      msg: '',
-      show: false,
-      cover: false,
-      type: 'text',
+    const handleClick = (type, msg, cover = false) => {
+      state.show = true;
+      state.msg2 = msg;
+      state.type = type;
+      state.cover = cover;
+    };
+
+
+    // 发起请求
+    const setRequest = () => {
+      api.clickRecord({ name: '点击测试' }).then(res => {
+        console.log(res);
+      });
+    }
+
+
+    // 微信分享
+    useShareAppMessage((res) => {
+      let data = {};
+      let user = store.user;
+      let title = `${user.nickName}向你推荐了小程序`;
+      let path = `/pages/notfind/index`;
+      // let imgUrl = 'https://img.server.com/picture/Beautyleg/05.25%20No.539%20Lynn%2068pics/0013.jpg';
+      if (res.from === "button") {
+        // 来自页面内转发按钮
+        // console.log(res.target.dataset)
+        data = res.target.dataset;
+      }
+      if (Object.keys(data).length > 0) {
+        title = `${user.nickName}向你推荐了${data.name}服务`;
+        path = `/pages/contact/detail?id=${user.id}&sid=${data.sid}`;
+      }
+      return {
+        title: title,
+        path: path,
+        // imageUrl: imgUrl,
+        /* 自定义图片路径，可以是本地文件路径、代码包文件路径或者网络图片路径。支持 PNG 及JPG。显示图片长宽比是 5:4。*/
+        // promise
+      };
     });
+
+
+    onMounted(() => {
+      // console.log(utils.global.getValue().version);
+    })
 
     return {
       ...toRefs(state),
-      ...toRefs(tips),
+      handleClick,
+      // 路由文档
+      // http://lblblib.gitee.io/tarojs-router-next/guide/quike/start
+      goPage: (url) => utils.router.goPage(url, { params: { id: 1 } }),
+      setRequest,
+      global: computed(() => utils.global.getValue())
     }
   }
 }
 </script>
 
 <style lang="scss">
-
+img {
+  display: block;
+  max-width: 100%;
+  margin: auto;
+}
 </style>
--- packages/nutui/src/pages/index/index.vue
+++ packages/nutui/src/pages/index/index.vue
@@ -1,70 +1,54 @@
 <template>
   <view class="index">
     <TopBar class="top-bar-bg" title="首页" :setPlace="true"></TopBar>
-    <!-- <view>
-      <img src="//img.server.com/pinterest/fbd7aff4f39ffa4a9f6f7b222b244237.jpg" :style="{ width: '100%' }" alt=""
-        mode="widthFix" />
-    </view> -->
+    <nut-swiper class="banner" :pagination-visible="true" pagination-color="#ffffff" auto-play="3000">
+      <nut-swiper-item v-for="(item, index) in banners" :key="index">
+        <img class="banner-img" :src="item.path" alt="" mode="widthFix" />
+      </nut-swiper-item>
+    </nut-swiper>
 
 
-    <NutCellGroup :title="wellcome" :desc="`版本号${global.version}`">
-      <NutCell title="404页面" is-link @click="goPage('/pages/notfind/index')"></NutCell>
-    </NutCellGroup>
+    <view class="container">
+      <view class="main-panel">
+        <view class="user-header">
+          <view class="welcome">
+            <view class="hello">Hello<span v-if="user != null">{{ user.nickName }}</span></view>
+            {{ msg }}
+          </view>
+          <div class="n-space"></div>
 
-
-
-
-    <NutCellGroup title='微信原生功能测试'>
-      <NutCell>
-        <NutSpace wrap>
-          <nut-button open-type="share">分享</nut-button>
-        </NutSpace>
-      </NutCell>
-    </NutCellGroup>
-    <NutCellGroup title='按钮效果'>
-      <NutCell>
-        <NutSpace wrap>
-          <nut-button type="primary" @click="handleClick('text', msg2, true)">信息提示toast</nut-button>
-          <nut-button @click="setRequest">发起测试请求</nut-button>
-        </NutSpace>
-      </NutCell>
-    </NutCellGroup>
-
-    <NutCellGroup title="第三方图标" desc="使用请打开app.js中remixicon.css注释。下方是图标效果。注意：使用后打包文件会增加100k">
-      <NutCell>
-        <NutSpace wrap>
-          <IconFont font-class-name="remixicon" class-prefix="ri" name="home-7-line"></IconFont>
-          <nut-button>
-            <template #icon>
-              <IconFont font-class-name="remixicon" class-prefix="ri" name="delete-bin-5-line"></IconFont>
-            </template>
+          <nut-button v-if="user == null" type="primary" @click="userLogin();">立即登录
           </nut-button>
-        </NutSpace>
-      </NutCell>
+          <template v-else>
+            <div @click="goPage('/settings/pages/user/baseInfo')">
+              <userAvatar ref="avatar"></userAvatar>
+            </div>
+          </template>
+        </view>
+        <nut-divider :style="{ color: '#dddddd', borderColor: '#dddddd' }" />
 
-    </NutCellGroup>
+        <view class="grid-links">
+          <nut-cell class="grid-item" v-for="(item, index) in gridLinks" :key="index" is-link center right-icon=""
+            @click="goPage(item.url);">
+            <IconFont :name="item.icon" class="icon" size="24"></IconFont>
+            <!-- <img class="icon" :src="item.icon" mode="widthFix" /> -->
+            <view>{{ item.title }}</view>
+          </nut-cell>
+        </view>
 
-    <NutCellGroup title="原始图标">
-      <NutCell>
-        <NutSpace wrap>
-          <IconFont name="JD"></IconFont>
-        </NutSpace>
-      </NutCell>
-    </NutCellGroup>
+      </view>
 
 
-    <nut-toast :msg="msg" v-model:visible="show" :type="type" :cover="cover" />
+    </view>
 
   </view>
 </template>
 
 <script>
-import { useShareAppMessage } from "@tarojs/taro";
 import { reactive, toRefs, onMounted, computed } from 'vue';
-import utils from "@/utils/index";
-import api from "@/api/";
-import { useSystemStore } from "@/store/index";
+import { useSystemStore } from '@/store/index';
 import { IconFont } from '@nutui/icons-vue-taro';
+import utils from "@/utils/index";
 
 export default {
   name: 'Index',
@@ -73,59 +57,84 @@ export default {
   },
   setup() {
 
-
-
     const store = useSystemStore();
 
+
+
+
     const state = reactive({
-      wellcome: '欢迎使用 NutUI 开发小程序',
-      msg: '你成功了～',
+      hasToken: false,
+      msg: '欢迎使用 NutUI 开发小程序',
+      msg2: '',
       type: 'text',
       show: false,
-      cover: false
+      cover: false,
+      user: computed(() => store.user),
+      banners: [{
+        path: 'https://storage.360buyimg.com/jdc-article/NutUItaro34.jpg',
+      }, {
+        path: 'https://storage.360buyimg.com/jdc-article/NutUItaro2.jpg',
+      }, {
+        path: 'https://storage.360buyimg.com/jdc-article/welcomenutui.jpg',
+      }, {
+        path: 'https://storage.360buyimg.com/jdc-article/fristfabu.jpg'
+      }],
+      gridLinks: [{
+        title: '校园超市',
+        name: 'supermarket',
+        icon: 'shop',
+        url: ''
+      },
+      {
+        title: '校园外卖',
+        name: 'takeout',
+        icon: 'dshop',
+        url: ''
+      },
+      {
+        title: '云打印',
+        name: 'cprint',
+        icon: 'footprint',
+        url: ''
+      },
+      {
+        title: '校园洗衣房',
+        name: 'laundryRoom',
+        icon: 'refresh2',
+        url: ''
+      }, {
+        title: '校园驾校',
+        name: 'drivingSchool',
+        icon: 'jdl',
+        url: ''
+      }, {
+        title: '电瓶车回购',
+        name: 'electricalRecycling',
+        icon: 'order',
+        url: ''
+      }, {
+        title: '快递代取',
+        name: 'courier',
+        icon: 'people',
+        url: ''
+      }, {
+        title: '跳蚤市场',
+        name: 'oldGoods',
+        icon: 'date',
+        url: ''
+      }
+      ]
     });
 
-    const handleClick = (type, msg, cover = false) => {
-      state.show = true;
-      state.msg2 = msg;
-      state.type = type;
-      state.cover = cover;
-    };
 
-
-    // 发起请求
-    const setRequest = () => {
-      api.clickRecord({ name: '点击测试' }).then(res => {
-        console.log(res);
+    // 登录按钮
+    const userLogin = () => {
+      // console.log(utils);
+      // 用户登录
+      utils.user.userLogin({}, () => {
+        utils.router.goHomeRelaunch(); // 重定位到首页
       });
     }
-
-
-    // 微信分享
-    useShareAppMessage((res) => {
-      let data = {};
-      let user = store.user;
-      let title = `${user.nickName}向你推荐了小程序`;
-      let path = `/pages/notfind/index`;
-      let imgUrl = 'https://img.server.com/picture/Beautyleg/05.25%20No.539%20Lynn%2068pics/0013.jpg';
-      if (res.from === "button") {
-        // 来自页面内转发按钮
-        // console.log(res.target.dataset)
-        data = res.target.dataset;
-      }
-      if (Object.keys(data).length > 0) {
-        title = `${user.nickName}向你推荐了${data.name}服务`;
-        path = `/pages/contact/detail?id=${user.id}&sid=${data.sid}`;
-      }
-      return {
-        title: title,
-        path: path,
-        imageUrl: imgUrl,
-        /* 自定义图片路径，可以是本地文件路径、代码包文件路径或者网络图片路径。支持 PNG 及JPG。显示图片长宽比是 5:4。*/
-        // promise
-      };
-    });
-
 
     onMounted(() => {
       // console.log(utils.global.getValue().version);
@@ -133,21 +142,130 @@ export default {
 
     return {
       ...toRefs(state),
-      handleClick,
-      // 路由文档
-      // http://lblblib.gitee.io/tarojs-router-next/guide/quike/start
-      goPage: (url) => utils.router.goPage(url, { params: { id: 1 } }),
-      setRequest,
-      global: computed(() => utils.global.getValue())
+      userLogin,
+      goPage: (url) => utils.router.goPage(url),
     }
   }
 }
 </script>
 
 <style lang="scss">
-img {
+.index {
+  .container {
+    position: relative;
+    z-index: 1;
+    // margin-top: -65px;
+    // margin-left: 15px;
+    // margin-right: 15px;
+  }
+
+
+}
+
+.banner {
   display: block;
-  max-width: 100%;
-  margin: auto;
+  width: 100%;
+  height: auto;
+  background: #aaa;
+  // aspect-ratio: 750/776;
+
+  .nut-swiper-item {
+    line-height: 150px;
+  }
+
+  .banner-img {
+    width: 100%;
+    height: 100%;
+  }
+
+  .nut-swiper-pagination {
+    // margin-bottom: 75px;
+
+    .h5-i {
+      opacity: 0.3;
+      background-color: #fff;
+    }
+
+    .active {
+      width: 28px;
+      opacity: 1;
+    }
+  }
+}
+
+.main-panel {
+  // background: #fff;
+  // box-shadow: 0px 0px 6px 5px rgba(0, 0, 0, 0.06);
+  // border-radius: 8px;
+  padding: 24px;
+  // min-height: 200px;
+  margin-bottom: 12px;
+}
+
+.user-header {
+  display: flex;
+  align-items: flex-end;
+
+  .welcome {
+    // padding-right: 60px;
+    line-height: 1.2;
+    max-width: 140px;
+  }
+
+  .hello {
+    padding-bottom: 8px;
+    display: flex;
+    gap: 8px;
+  }
+}
+
+.grid-links {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: auto;
+  gap: 12px 16px;
+
+  .grid-item {
+    flex-direction: column;
+    padding: 8px 0;
+    margin: 0;
+    box-shadow: none;
+    justify-content: center;
+    font-size: 12px;
+    gap: 8px;
+    box-shadow: 0px 0px 10px 5px rgba(0, 0, 0, 0.06);
+  }
+
+  .icon {
+    width: 44px;
+    height: 44px;
+  }
+}
+
+.img-card {
+  overflow: hidden;
+  display: block;
+  box-shadow: 0px 0px 6px 5px rgba(0, 0, 0, 0.06);
+  border-radius: 6px;
+  background: #fff;
+  padding: 0;
+  margin: 0 0 12px;
+
+  .img {
+    display: block;
+    width: 100%;
+    height: 108px;
+  }
+}
+
+
+.message-pop {
+  width: 80%;
+  padding: 20px;
+
+  .item-content {
+    flex-direction: column;
+    gap: 4px;
+  }
 }
 </style>

0f3c8bc6da8b25aa5ffd210d89c6195089ae9ee4
hy <iqszlong@qq.com>
2023-12-11 13:01:22 +0800

    Update imgRes.js

--- utils/js/imgRes.js
+++ utils/js/imgRes.js
@@ -1,5 +1,5 @@
 //预览图片路径
-const previewMediaPath = (path,size='500x304',bg='ddd',color='555',text='') => {
+const previewMediaPath = (path,{size='500x304',bg='ddd',color='555',text=''}={}) => {
     let pathStr = "";
     if (typeof path === "undefined" || path === "") {
       pathStr = `//fakeimg.pl/${size}/${bg}/${color}/?text=${text}&font=noto`;
